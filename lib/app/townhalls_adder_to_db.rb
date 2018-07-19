b9949ae10c88deac691ba77e51902a309100e1f5
Dyf-Tfh <n.zhao@hotmail.fr>
2018-07-19 17:16:06 +0200

    classification

--- lib/app/townhalls_adder_to_db.rb
+++ lib/app/townhalls_adder_to_db.rb
@@ -4,31 +4,44 @@ require 'twitter'
 require 'dotenv'
 require 'csv'
 Dotenv.load('../../.env')
-
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV["CONSUMER_KEY"]
-  config.consumer_secret     = ENV["CONSUMER_KEY_SECRET"]
-  config.access_token        = ENV["ACCESS_TOKEN"]
-  config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
-end
-
-# input all the names here or grab them from your database or somewhere else.
-# créer un tableau avec l'array de commune en ajoutant devant "mairie"
-names = CSV.read('../../db/scrapped_data.csv')[1].map{|x| "mairie " + x}
-
-user = []
-names.each do |name|
-  if client.user_search(name)[0] == nil
-    puts client.user_search(name)[0]
-    user << " "
-  else
-   user << client.user_search(name)[0].name
-   puts client.user_search(name)[0].name
+class Scapper_twitter
+  def connect
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["CONSUMER_KEY"]
+      config.consumer_secret     = ENV["CONSUMER_KEY_SECRET"]
+      config.access_token        = ENV["ACCESS_TOKEN"]
+      config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+    end
+    return client
   end
-end 
 
-# Ajoute l'array de compte twitter au csv
-CSV.open('../../db/scrapped_data.csv',"ab") do |csv| 
- csv << user
+
+  # créer un tableau avec l'array de commune en ajoutant devant "mairie"
+  # return un tableau
+  def scrap(client)
+  names = CSV.read('../../db/scrapped_data.csv')[1].map{|x| "mairie " + x}
+  user = []
+    names.each do |name|
+      if client.user_search(name)[0] == nil
+        puts client.user_search(name)[0]
+        user << " "
+      else
+      user << client.user_search(name)[0].screen_name
+      puts client.user_search(name)[0].screen_name
+      end
+    end
+    return user
+  end 
+  
+  def write(user)
+    # Ajoute l'array de compte twitter au csv
+    CSV.open('../../db/scrapped_data.csv',"ab") do |csv| 
+    csv << user
+    end
+  end
+  
+  def boucle
+    write(scrap(connect))
+  end
 end
-
+Scapper_twitter.new.boucle
